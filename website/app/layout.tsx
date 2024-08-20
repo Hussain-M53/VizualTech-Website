f7e4c42113481f10cdc1e5b57d6fbb026ce63af0
Hussain-M53 <hash.hussain53@gmail.com>
2024-08-21 00:16:43 +0500

    project structure setup

---
+++ website/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Roboto_Slab ,Inter} from "next/font/google";
+import "./globals.css";
+
+const inter = Roboto_Slab({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "VizualTech Solutions",
+  description: "Developed By DigiLabs.Co",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ website/app/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Navbar from "./_Components/navbar";
+import Hero from "./_Components/hero";
+import Values from "./_Components/values";
+import CTA from "./_Components/cta";
+import Testimonial from "./_Components/testimonial";
+import LogoCloud from "./_Components/logoCloud";
+import Footer from "./_Components/footer";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Values />
+      <Testimonial />
+      <CTA />
+      <LogoCloud /> 
+      <Footer/>
+    </div>
+  );
+}
