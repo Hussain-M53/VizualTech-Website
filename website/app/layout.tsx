b84578b4eb265bb0831df8f5450b6eb4ab5d9564
Hussain-M53 <hash.hussain53@gmail.com>
2024-08-23 23:04:29 +0500

    structure finalized

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -1,18 +1,15 @@
-'use client';
 import type { Metadata } from "next";
-import { Roboto_Slab ,Inter} from "next/font/google";
+import { Roboto_Slab, Inter } from "next/font/google";
 import "./globals.css";
-import { ReactElement, JSXElementConstructor } from "react";
-import { NavbarProps } from "sanity";
 import Navbar from "./_Components/navbar";
 import Footer from "./_Components/footer";
 
 const inter = Roboto_Slab({ subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   title: "VizualTech Solutions",
-//   description: "Developed By DigiLabs.Co",
-// };
+export const metadata: Metadata = {
+  title: "VizualTech Solutions",
+  description: "Developed By DigiLabs.Co",
+};
 
 export default function RootLayout({
   children,
@@ -22,10 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Navbar />  
-        
+        <Navbar />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
